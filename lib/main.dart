09399e7cb57b6058d40038a8ef20aefc5a758621
Youhei Oonuma <oonuma617@gmail.com>
2022-04-21 23:14:43 +0900

    sign in 画面を作成

--- lib/main.dart
+++ lib/main.dart
@@ -25,50 +25,79 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Photo App',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-              const SizedBox(height: 16),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'メールアドレス'),
-                keyboardType: TextInputType.emailAddress,
-              ),
-              const SizedBox(height: 8),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'パスワード'),
-                keyboardType: TextInputType.visiblePassword,
-                obscureText: true,
-              ),
-              const SizedBox(height: 16),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () => {},
-                  child: const Text('ログイン'),
+      body: Form(
+        key: _formKey,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Photo App',
+                  style: Theme.of(context).textTheme.headline4,
                 ),
-              ),
-              const SizedBox(height: 8),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () => {},
-                  child: const Text('新規登録'),
+                const SizedBox(height: 16),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'メールアドレス'),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (String? value) {
+                    if (value?.isEmpty == true) {
+                      return 'メールアドレスを入力してください';
+                    }
+                    return null;
+                  },
                 ),
-              ),
-            ],
+                const SizedBox(height: 8),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'パスワード'),
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
+                  validator: (String? value) {
+                    if (value?.isEmpty == true) {
+                      return 'パスワードを入力してください';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () => _onSinIn(),
+                    child: const Text('ログイン'),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () => _onSinUp(),
+                    child: const Text('新規登録'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  void _onSinIn() {
+    if (_formKey.currentState?.validate() != true) {
+      return;
+    }
+  }
+
+  void _onSinUp() {
+    if (_formKey.currentState?.validate() != true) {
+      return;
+    }
   }
 }
