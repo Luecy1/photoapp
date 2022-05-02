cd7d4f454d86684f99dd508cc452ae6a6588cc5d
Youhei Oonuma <oonuma617@gmail.com>
2022-05-02 23:25:04 +0900

    google account sign in

--- lib/sign_in_screen.dart
+++ lib/sign_in_screen.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:photoapp/photo_list_screen.dart';
 
 class SignInScreen extends StatefulWidget {
@@ -15,71 +16,83 @@ class _SignInScreenState extends State<SignInScreen> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
 
+  bool loading = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Form(
         key: _formKey,
         child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Photo App',
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-                const SizedBox(height: 16),
-                TextFormField(
-                  controller: _emailController,
-                  decoration: const InputDecoration(labelText: 'メールアドレス'),
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (String? value) {
-                    if (value?.isEmpty == true) {
-                      return 'メールアドレスを入力してください';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 8),
-                TextFormField(
-                  controller: _passwordController,
-                  decoration: const InputDecoration(labelText: 'パスワード'),
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: true,
-                  validator: (String? value) {
-                    if (value?.isEmpty == true) {
-                      return 'パスワードを入力してください';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 16),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () => _onSinIn(),
-                    child: const Text('ログイン'),
+          child: loading
+              ? const CircularProgressIndicator()
+              : Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Photo App',
+                        style: Theme.of(context).textTheme.headline4,
+                      ),
+                      const SizedBox(height: 16),
+                      TextFormField(
+                        controller: _emailController,
+                        decoration: const InputDecoration(labelText: 'メールアドレス'),
+                        keyboardType: TextInputType.emailAddress,
+                        validator: (String? value) {
+                          if (value?.isEmpty == true) {
+                            return 'メールアドレスを入力してください';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 8),
+                      TextFormField(
+                        controller: _passwordController,
+                        decoration: const InputDecoration(labelText: 'パスワード'),
+                        keyboardType: TextInputType.visiblePassword,
+                        obscureText: true,
+                        validator: (String? value) {
+                          if (value?.isEmpty == true) {
+                            return 'パスワードを入力してください';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 16),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () => _onSignIn(),
+                          child: const Text('ログイン'),
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () => _onSignUp(),
+                          child: const Text('新規登録'),
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () => _onGoolgeSignUp(),
+                          child: const Text('Google'),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                const SizedBox(height: 8),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () => _onSinUp(),
-                    child: const Text('新規登録'),
-                  ),
-                ),
-              ],
-            ),
-          ),
         ),
       ),
     );
   }
 
-  Future<void> _onSinIn() async {
+  Future<void> _onSignIn() async {
     try {
       if (_formKey.currentState?.validate() != true) {
         return;
@@ -97,17 +110,18 @@ class _SignInScreenState extends State<SignInScreen> {
     } catch (e) {
       print(e);
       showDialog(
-          context: context,
-          builder: (context) {
-            return AlertDialog(
-              title: Text('エラー'),
-              content: Text(e.toString()),
-            );
-          });
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('エラー'),
+            content: Text(e.toString()),
+          );
+        },
+      );
     }
   }
 
-  Future<void> _onSinUp() async {
+  Future<void> _onSignUp() async {
     try {
       if (_formKey.currentState?.validate() != true) {
         return;
@@ -132,5 +146,32 @@ class _SignInScreenState extends State<SignInScreen> {
             );
           });
     }
+  }
+
+  Future<void> _onGoolgeSignUp() async {
+    setState(() {
+      loading = true;
+    });
+
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    final userCredential =
+        await FirebaseAuth.instance.signInWithCredential(credential);
+
+    await Navigator.of(context).pushReplacement(MaterialPageRoute(
+      builder: (_) => PhotoListScreen(),
+    ));
+
+    setState(() {
+      loading = false;
+    });
   }
 }
